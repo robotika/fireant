68d6ff136f8ea8409fa6630ba02322e1f08112a5
martind <martind@mapfactor.com>
2013-12-12 08:07:40 +0100

    experiment with SPI servos (via Orion library)

--- servo_test/servo_test.ino
+++ servo_test/servo_test.ino
@@ -1,9 +1,13 @@
-
 // FireAnt servo test
 //   robotika.cz
 #include <BMServo.h>
+#include <Orion.h>
+#include <SPI.h>
 
-const int SERVO_PIN = 10;
+
+const int ARDUINO_SERVO_PIN = 10;
+const int SPI_SERVO_PIN = 0;
+
 
 const int BUTTON_PIN = 2;
 const int ledPin =  A1;      // the number of the LED pin
@@ -14,7 +18,7 @@ int angle = 0;
 // variables will change:
 int buttonState = 0;         // variable for reading the pushbutton status
 
-BMServo servo( SERVO_PIN );
+BMServo servo( ARDUINO_SERVO_PIN );
 
 void setup()
 {
@@ -22,6 +26,12 @@ void setup()
   pinMode( BUTTON_PIN, INPUT);
   servo.begin();
   servo.setAngle( angle, 1000 );
+  
+  Orion.begin(); 
+  Orion.tone(NOTE_C6,100); 
+  Orion.setAngle( SPI_SERVO_PIN, angle );
+  Orion.setServoMin( SPI_SERVO_PIN, -900 );
+  Orion.setServoMax( SPI_SERVO_PIN, 900 );
 }
 
 void loop()
@@ -30,15 +40,19 @@ void loop()
 
   buttonState = digitalRead( BUTTON_PIN );
 
+  Orion.setTime( 1000 );
   if (buttonState == HIGH) {     
     // turn LED on:    
     digitalWrite(ledPin, HIGH);  
     servo.setAngle( 200, 1000 );
+    Orion.setAngle( SPI_SERVO_PIN, 900 );
   } 
   else {
     // turn LED off:
     digitalWrite(ledPin, LOW); 
     servo.setAngle( -200, 1000 );
+    Orion.setAngle( SPI_SERVO_PIN, -900 );
   }
+  Orion.execute();
 }
 
