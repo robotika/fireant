a28bcea7d1f461708201f31d02246f784e5cd581
martind <martind@mapfactor.com>
2013-12-15 13:05:59 +0100

    added option to reply log

--- serial_servo/serial_servo.py
+++ serial_servo/serial_servo.py
@@ -2,6 +2,7 @@
   Simple read/write utility for remote control of servos on FireAnt
 """
 
+import sys
 import serial 
 import datetime
 
@@ -34,12 +35,55 @@ class LogIt():
     self._logFile.flush()
     self._com.write( chars ) 
 
-def main():
-  com = LogIt( serial.Serial( 'COM8',9600 ) )
+#-------------------------------------------------------------------
+
+class ReplyLog():
+  "Read & verify log"
+  def __init__( self, filename, assertWrite=True ):
+    print "ReplyLog", filename
+    self._logFile = open( filename, "rb" )
+    self.assertWrite = assertWrite
+
+  def read( self, numChars ):
+    s = []
+    for i in range(numChars):
+      marker = self._logFile.read(1)
+      if not marker:
+        raise LogEnd()
+      assert( marker == chr(0x01) )
+      s.append(self._logFile.read(1))
+      if not s[-1]:
+        raise LogEnd()
+    return ''.join(s)
+
+  def write( self, chars ):
+    for ch in chars:
+      marker = self._logFile.read(1)
+      if not marker:
+        raise LogEnd()
+      assert( marker == chr(0x00) )
+      verifyCh = self._logFile.read(1)
+      if not verifyCh:
+        raise LogEnd()
+      if self.assertWrite:
+        assert( verifyCh == ch ) 
+
+#-------------------------------------------------------------------
+
+
+
+def main( filename=None ):
+  if filename:
+    com = ReplyLog( filename )
+  else:
+    com = LogIt( serial.Serial( 'COM8',9600 ) )
   for i in xrange(100):
     com.read(1)
 
 
 if __name__ == "__main__":
-  main()
+  if len(sys.argv) > 1:
+    main( sys.argv[1] )
+  else:
+    main()
 
