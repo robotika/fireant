7e36375cd99d62b75436ff27f0cb1c0ab89b1205
martind <martind@mapfactor.com>
2013-12-15 15:50:11 +0100

    first experiment with "identity", but not properly working yet

--- serial_servo/serial_servo.ino
+++ serial_servo/serial_servo.ino
@@ -42,6 +42,60 @@ void setup()
   }
 }
 
+void sendServos()
+{
+  int i;
+  Serial.write( START_BLOCK );
+  for( i = 0; i < NUM_SERVOS; i++ )
+  {
+    int angle = Orion.queryFBAngle( servoPins[i] );    
+    if( angle < SERVO_MIN || angle > SERVO_MAX )
+      Serial.write( SERVO_OUT_OF_RANGE );
+    else
+      Serial.write( (unsigned char)(angle/10) );
+  }  
+  Serial.write( END_BLOCK );
+}
+
+int blockedRead()
+{
+  // not good solution ... just quick wrapper
+  int b;
+  b = Serial.read();
+  while( b < 0 )
+    b = Serial.read();
+  return b;
+}
+
+void receiveServos()
+{
+  int b;
+  int i;
+  Orion.green(true);
+  Orion.setTime( 1000 );
+  b = blockedRead();
+  while( true )
+  {
+    while( b != START_BLOCK )
+      b = blockedRead();
+    b = blockedRead();
+    i = 0;
+    while( b != START_BLOCK && b != END_BLOCK && i < NUM_SERVOS )
+    {
+      if( b == SERVO_OUT_OF_RANGE )
+        Orion.stopPulse( servoPins[i] );
+      else
+        Orion.setAngle( servoPins[i], (int)((signed char)b)*10 );
+      b = blockedRead();
+      i++;
+    }
+    if( b == END_BLOCK )
+      break;
+  }
+  Orion.execute();
+  Orion.green(false);
+}
+
 void loop()
 {
   int i;
@@ -51,17 +105,7 @@ void loop()
       Orion.stopPulse( servoPins[i] ); // it is by default, but this way it is more clear
     return; //Battery too low to do anything.
   }
-  
-  Serial.write( START_BLOCK );
-  for( i = 0; i < NUM_SERVOS; i++ )
-  {
-    int angle = Orion.queryFBAngle( servoPins[i] );    
-    if( angle < SERVO_MIN || angle > SERVO_MAX )
-      Serial.write( SERVO_OUT_OF_RANGE );
-    else
-      Serial.write( (unsigned char)(angle/10) );
-  }
-  
-  Serial.write( END_BLOCK );
+  sendServos();
+  receiveServos();  
 }
 
