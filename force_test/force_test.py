4169ed5e2ca264480596a7e5a4a62d48b817d175
martind <martind@mapfactor.com>
2013-12-20 21:32:10 +0100

    parsing RobotStatus packets

--- force_test/force_test.py
+++ force_test/force_test.py
@@ -9,12 +9,28 @@ import datetime
 import ctypes
 import math
 
+import struct
+
+
 import os
 sys.path.append( ".."+os.sep+"serial_servo") 
 from serial_servo import LogIt, ReplyLog
 
+PACKET_START = chr(0xAB)
+ECHO_CHAR = 'D'
+
 def readRobotStatus( com, verbose ):
-  pass
+  while com.read(1) != PACKET_START:
+    pass
+  size = ord(com.read(1))
+  chSum = size;
+  buf = com.read( size )
+  assert (size+sum([ord(x) for x in buf])) % 256 == 0, [hex(ord(x)) for x in buf]
+  raw = struct.unpack_from( ">HHhhhhhh", buf ) # big indian
+  if verbose:
+    print raw
+  
+
 
 def main( filename=None ):
   if filename:
@@ -23,8 +39,9 @@ def main( filename=None ):
   else:
     com = LogIt( serial.Serial( 'COM8',9600 ) )
     verbose = False
-  for i in xrange(1000):
-    com.read(1)
+
+  for i in xrange(30):
+    readRobotStatus( com, verbose )
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
