451256a7c3ca1370a1b21ac95173fef5d47d89b4
martind <martind@mapfactor.com>
2014-01-28 06:52:10 +0100

    experimenting with servo min/max/dir/offset

--- minmax/minmax.ino
+++ minmax/minmax.ino
@@ -14,6 +14,11 @@ void setup()
   Orion.tone(NOTE_C6,100); 
   Orion.tone(NOTE_E6,100); 
   delay(100); 
+  Orion.setServoMin( SERVO_PIN, -2000 );
+  Orion.setServoMax( SERVO_PIN, 2000 );
+  Orion.setServoDir( SERVO_PIN, 0);
+  Orion.setAOffset( SERVO_PIN, 100 );
+  delay(100); 
 }
 
 void loop()
@@ -24,7 +29,7 @@ void loop()
     return;
   } 
   Orion.setTime( 100 ); 
-  Orion.setAngle( SERVO_PIN, 0 );
+  Orion.setAngle( SERVO_PIN, 200 );
   Orion.execute(); 
   delay(10); 
 }
