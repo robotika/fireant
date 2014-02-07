b943029d784a52debb9c837db68c022ab95396d8
martind <martind@mapfactor.com>
2014-02-07 07:38:40 +0100

    slow down serial output

--- test_ps2/test_ps2.ino
+++ test_ps2/test_ps2.ino
@@ -1,3 +1,4 @@
+// Simple test of PlayStation2 interface
 #include <BMSerial.h>
 #include <BMPS2X.h>
 
@@ -5,7 +6,7 @@ BMPS2 ps2x(6,100);
 
 void setup()
 {
-  Serial.begin(625000);
+  Serial.begin(9600);
   //Clears pressed/released states
   ps2x.explicitReadsOnly();
   ps2x.read_ps2(true);
