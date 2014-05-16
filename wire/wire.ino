f05e11a40a57875f7476f73b43bdd4ad7e54a798
martind <martind@mapfactor.com>
2014-05-16 06:42:57 +0200

    experimental both USB and Bluetooth wiring to communicate with serial shield

--- wire/wire.ino
+++ wire/wire.ino
@@ -4,44 +4,49 @@
 #define GREENLED A1
 #define REDLED A0
 
-//#define RXD 0
-//#define TXD 1
+#define RXD 0
+#define TXD 1
 
 // Bluetooth pins
-#define RXD 8
-#define TXD 9
+#define BT_RXD 8
+#define BT_TXD 9
 
 void setup()
 {
   pinMode( GREENLED, OUTPUT );
   pinMode( REDLED, OUTPUT );
   pinMode( TXD, OUTPUT );
-  pinMode( 1, OUTPUT );
+  pinMode( BT_TXD, OUTPUT );
+  pinMode( MOSI, OUTPUT );
+  pinMode( BT_RXD, INPUT );
+  digitalWrite( BT_RXD, HIGH ); // pullup required for Bluetooth module
   pinMode( RXD, INPUT );
-  digitalWrite( RXD, HIGH ); // pullup required for Bluetooth module
+  digitalWrite( RXD, HIGH ); // pullup (to handle not connected pin(?))  
 }
 
 void loop()
 {
-  if( digitalRead( 0 ) == LOW )
+  if( digitalRead( MISO ) == LOW )
   {
-    digitalWrite( GREENLED, LOW );
     digitalWrite( TXD, LOW );
-  }
-  else
-  {
+    digitalWrite( BT_TXD, LOW );
     digitalWrite( GREENLED, HIGH );
-    digitalWrite( TXD, HIGH );
-  }
-  if( digitalRead( RXD ) == LOW )
-  {
-    digitalWrite( REDLED, LOW );
-    digitalWrite( 1, LOW );
   }
   else
   {
+    digitalWrite( TXD, HIGH );
+    digitalWrite( BT_TXD, HIGH );
+    digitalWrite( GREENLED, LOW );
+  }
+  if( digitalRead( RXD ) == LOW || digitalRead( BT_RXD ) == LOW )
+  {
+    digitalWrite( MOSI, LOW );
     digitalWrite( REDLED, HIGH );
-    digitalWrite( 1, HIGH );
+  }
+  else
+  {
+    digitalWrite( MOSI, HIGH );
+    digitalWrite( REDLED, LOW );
   }
 }
 
