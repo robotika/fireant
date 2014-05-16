a2ab0d820a4f5ba9d19cec906c69392c3ac6213d
martind <martind@mapfactor.com>
2014-05-16 05:02:10 +0200

    Bluetooth - removed dependency on Orion and removed ECHO

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -2,8 +2,6 @@
 // console for mirroing input and output
 
 #include <BMSerial.h>
-#include <Orion.h>
-#include <SPI.h> 
 
 BMSerial bluetooth(8,9);
 
@@ -11,27 +9,17 @@ void setup()
 {
   Serial.begin( 9600 );
   Serial.println( "BlueTooth TEST" ); 
-  Orion.begin();
-  Orion.tone(NOTE_C4,100);  
-  Orion.tone(NOTE_D4,100);
   bluetooth.begin( 9600 );
 }
 
 void loop()
 {
-  if(Orion.checkLipo())
-  {
-    return; //Battery too low to do anything.
-  } 
   if( bluetooth.available() )
     Serial.write( bluetooth.read() );
     
   int b;
   b = Serial.read(); 
   if( b >= 0 )
-  {
-    Serial.write( b ); // ECHO
     bluetooth.write( b );
-  }
 }
 
